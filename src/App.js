898a713cea72494eb51326961c58d6970902e6b4
jamesbond100 <alexfong100@live.com>
2019-06-20 14:50:01 +0800

    Code Cleanup!

--- src/App.js
+++ src/App.js
@@ -14,27 +14,9 @@ const Home = () => (
   <MTable></MTable>
 )
 
-const About = () => (
+const FinancialOverview = () => (
   <div>
-    About
-  </div>
-)
-
-const Code = () => (
-  <div>
-    Code
-  </div>
-)
-
-const Contact = () => (
-  <div>
-    Contact
-  </div>
-)
-
-const info = () => (
-  <div>
-    info
+    FinancialOverview
   </div>
 )
 
@@ -43,19 +25,11 @@ class MainMenu extends Component {
     return (
       <div>
         <Link to="/">
-          <button>home</button>
+          <button>Timetable</button>
         </Link>
-        <Link to="/about">
-          <button>About</button>
-        </Link>
-        <Link to="/code">
-          <button>code</button>
-        </Link>
-        <Link to="/code">
-          <button>contact</button>
-        </Link>
-        <Link to="/info">
-          <button>info</button>
+
+        <Link to="/FinancialOverview">
+          <button>Financial Overview</button>
         </Link>
       </div>
     );
@@ -71,11 +45,7 @@ class App extends Component {
             <MainMenu/>
           <div>
             <Route exact path="/" component={Home} />
-            <Route exact path="/about" component={About} />
-            <Route exact path="/code" component={Code} />
-            <Route exact path="/contact" component={Contact} />
-            <Route exact path="/info" component={info} />
-            <Route exact path="/timetable" component={MTable} />
+            <Route exact path="/FinancialOverview" component={FinancialOverview} />
           </div>
         </div>
       </Router>
