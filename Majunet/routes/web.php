271fdc00cfa315c7f7a75e64c1524c9871898c2a
Kishan Nirghin <kishanjay@live.nl>
2019-06-24 11:42:48 +0800

    Updates router

--- Majunet/routes/web.php
+++ Majunet/routes/web.php
@@ -11,14 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/{path?}', 'app');
+
 Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-
-Route::get("/majunet", function() {
-    return view('majunet');
-});
