382b911c96c59ee4d9b9071caf48ea8b3f9ca88e
Kishan Nirghin <kishanjay@live.nl>
2019-06-25 15:40:50 +0800

    Fixes employee table

--- resources/js/pages/MEmployee.js
+++ resources/js/pages/MEmployee.js
@@ -36,18 +36,18 @@ class MEmployee extends Component {
                         <td>Nama Penuh</td>
                         <td># Telefon</td>
                         <td>Alamat</td>
-                        <td># IC</td>
+                        <td>City</td>
                         </tr>
                 </thead>
 
                 <tbody>
                         {this.state.employees.map(emp_name =>
                         <tr>
-                            <td>{counter++}</td>
+                            <td>{emp_name.id}</td>
                             <td>{emp_name.first_name}</td>
                             <td>{emp_name.phone}</td>
                             <td>{emp_name.address}</td>
-                            <td>{emp_name.id}</td>
+                            <td>{emp_name.city}</td>
                         </tr>)}
                 </tbody>
             </table>
