89c1e6d560a0e74c982dc2b2f13e6c572a69a9a5
jamesbond100 <alexfong100@live.com>
2019-06-27 21:07:40 +0800

    Able to receive employee id from MEmployee.js

--- resources/js/pages/MEmpDetails.js
+++ resources/js/pages/MEmpDetails.js
@@ -1,17 +1,39 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
+
+
 
 class MEmpDetails extends Component {
-    render() { 
+    constructor(props){
+        super(props);
+    }
+    
+    componentDidMount(){
+        const { empId } = this.props.location.state;
+        console.log(empId);
+    }
+    render() {
+        console.log("Props: ", this.props.empId);
         return ( 
             <div class="container">
+                <nav class="breadcrumb has-succeeds-separator" aria-label="breadcrumbs">
+                <ul>
+                    <li><Link to="/Employee">
+                            Employees
+                        </Link></li>
+                    <li class="is-active"><a href="#" aria-current="page">Employees Details</a></li>
+                </ul>
+                </nav>
+
                 <figure class="image is-128x128">
-                    <img src={require('./logo.png')}/>
+                    <img src={require('../components/logo.png')}/>
                 </figure>
 
                 <table className="table is-hoverable">
                     <tr>
                         <td><p>Nama Penuh:</p></td>
                         <td><input class="input" type="text" placeholder="Text input" disabled/></td>
+                        
                     </tr>
 
                     <tr>
@@ -27,6 +49,10 @@ class MEmpDetails extends Component {
                     <tr>
                         <td><p>No. Telefon:</p></td>
                         <td><input class="input" type="text" placeholder="Text input"/></td>
+                    </tr>
+
+                    <tr>
+                    <td>here here!!{this.props.empId}</td>
                     </tr>
                     
                 </table>
