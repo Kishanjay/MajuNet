4d2427924ae672384b98b7ab1fe44edebf54518b
jamesbond100 <alexfong100@live.com>
2019-06-28 13:26:51 +0800

    Translated to malay language

--- resources/js/pages/MHome.js
+++ resources/js/pages/MHome.js
@@ -13,7 +13,8 @@ const MHeader = () => (
                         MajuNet
                 </h1>
                     <h2 className="subtitle">
-                        Explore, Expand, Experience local food of Sarawak
+                        {/* Explore, Expand, Experience local food of Sarawak */}
+                        Meneroka, Mengembang dan Mengalami makanan tempatan Sarawak
                  </h2>
                 </div>
             </div>
@@ -22,9 +23,10 @@ const MHeader = () => (
     
             <section className="section">
                 <div className="container">
-                    <h1 className="title">About</h1>
+                    <h1 className="title">Tentang Majunet</h1> {/* What is About MajuNet*/}
                     <h2 className="subtitle">
-                    The main purpose of the system is supporting the Women's Institute (WI) in repackaging local products from Sarawak. The WI is the central stakeholder that employs women from  Kampung Pinggan Jaya and Muhibbah. They will repackage and rebrand the local product popiah simpul, that will be exported by a third party, AlphaBees, to Singapore. The system supports the WI in scheduling women and keep track of the financials from requested orders.
+                    {/* The main purpose of the system is supporting the Women's Institute (WI) in repackaging local products from Sarawak. The WI is the central stakeholder that employs women from  Kampung Pinggan Jaya and Muhibbah. They will repackage and rebrand the local product popiah simpul, that will be exported by a third party, AlphaBees, to Singapore. The system supports the WI in scheduling women and keep track of the financials from requested orders. */}
+                    Tujuan utama sistem ini adalah untuk membantu Institut Wanita (IW) dalam proses pembungkusan produk tempatan Sarawak, dimana IW adalah pihak utama yang menggaji wanita dari Kampung Pinggan Jaya dan Kampung Muhibbah. Wanita-wanita tersebut akan melakukan kerja pembungkusan dan penjenamaan semula popiah simpul. Seterusnya produk tersebut akan dieksport oleh pihak ketiga (AlphaBees) ke Singapura. Sistem ini mampu membantu IW dalam penjadualan kerja dan keterangan kewangan.
             </h2>
                 </div>
             </section>
